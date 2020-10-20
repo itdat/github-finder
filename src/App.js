ba459ec72bd234af62fda559a5126b30aa7db4ca
Nguyen Tuan Dat <53074785+itdat@users.noreply.github.com>
2020-10-21 00:01:56 +0700

    Refactor to display users from API

--- src/App.js
+++ src/App.js
@@ -2,16 +2,38 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="container">
-        <Users />
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      loading: false,
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({
+      users: res.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { users, loading } = this.state;
+
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Users loading={loading} users={users} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,43 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
 
-class Users extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [
-        {
-          id: 1,
-          login: "mojombo",
-          avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-          html_url: "https://github.com/mojombo",
-        },
-        {
-          id: 2,
-          login: "defunkt",
-          avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-          html_url: "https://github.com/defunkt",
-        },
-        {
-          id: 3,
-          login: "pjhyett",
-          avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-          html_url: "https://github.com/pjhyett",
-        },
-      ],
-    };
-  }
-
-  render() {
+const Users = ({ loading, users }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user, i) => (
+        {users.map((user, i) => (
           <UserItem key={i} avatar_url={user.avatar_url} login={user.login} html_url={user.html_url} />
         ))}
       </div>
     );
   }
-}
+};
 
 const userStyle = {
   display: "grid",
